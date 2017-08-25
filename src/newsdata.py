8e73ae32a39c985d5052dc18d44defb12cb6c02a
Young Wook Baek <kywbaek@gmail.com>
2017-08-25 18:14:22 +0100

    add helper function to remove code duplicate in newsdata.py

--- src/newsdata.py
+++ src/newsdata.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import psycopg2
 import sys
 
@@ -12,31 +13,46 @@ def create_view():
     DB.close()
 
 
+def execute_query(query):
+    """execute_query takes an SQL query as a parameter.
+       Executes the query and returns the results as a list of tuples.
+      args:
+          query - an SQL query statement to be executed.
+      returns:
+          A list of tuples containing the results of the query.
+   """
+    try:
+        DB = psycopg2.connect("dbname=news")
+        c = DB.cursor()
+        c.execute(query)
+        result = c.fetchall()
+        DB.close()
+        return result
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
 def question1():
-    DB = psycopg2.connect("dbname=news")
-    c = DB.cursor()
-    c.execute(
-        "SELECT title, COUNT(*) AS views FROM articles_log GROUP BY title ORDER BY views DESC LIMIT 3")
-    solution = [('\"{}\" - {} views\n'.format(tup[0], tup[1])) for tup in c.fetchall()]
-    DB.close()
+    query = """SELECT title, COUNT(*) AS views FROM articles_log GROUP BY title ORDER BY views DESC LIMIT 3"""
+    result = execute_query(query)
+    solution = [('\"{}\" - {} views\n'.format(tup[0], tup[1]))
+                for tup in result]
     return solution
 
 
 def question2():
-    DB = psycopg2.connect("dbname=news")
-    c = DB.cursor()
-    c.execute("SELECT name, COUNT(*) AS views FROM articles_log, authors WHERE authors.id = articles_log.author GROUP BY name ORDER BY views DESC")
-    solution = [('{} - {} views\n'.format(tup[0], tup[1])) for tup in c.fetchall()]
-    DB.close()
+    query = """SELECT name, COUNT(*) AS views FROM articles_log, authors WHERE authors.id = articles_log.author GROUP BY name ORDER BY views DESC"""
+    result = execute_query(query)
+    solution = [('{} - {} views\n'.format(tup[0], tup[1]))
+                for tup in result]
     return solution
 
 
 def question3():
-    DB = psycopg2.connect("dbname=news")
-    c = DB.cursor()
-    c.execute("SELECT * from (SELECT log.time:: date AS day, ROUND(100.0 * (SUM(CASE WHEN status SIMILAR TO '(4|5)%' THEN 1 ELSE 0 END)) / COUNT(*), 1) AS errors FROM log GROUP BY day) AS errors_all WHERE errors > 1.0")
-    solution = [('{} - {}% errors\n'.format(tup[0], tup[1])) for tup in c.fetchall()]
-    DB.close()
+    query = """SELECT * FROM (SELECT log.time:: date AS day, ROUND(100.0 * (SUM(CASE WHEN status SIMILAR TO '(4|5)%' THEN 1 ELSE 0 END)) / COUNT(*), 1) AS errors FROM log GROUP BY day) AS errors_all WHERE errors > 1.0"""
+    result = execute_query(query)
+    solution = [('{} - {}% errors\n'.format(tup[0], tup[1]))
+                for tup in result]
     return solution
 
 
