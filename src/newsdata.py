5fe96edaa8c303f2324ee05c59203c084109a27b
Young Wook Baek <kywbaek@gmail.com>
2017-08-25 18:48:06 +0100

    add docstrings for functions in newsdata.py

--- src/newsdata.py
+++ src/newsdata.py
@@ -6,6 +6,8 @@ import sys
 
 
 def create_view():
+    """Create VIEW by executing the query
+    """
     DB = psycopg2.connect("dbname=news")
     c = DB.cursor()
     c.execute("CREATE VIEW articles_log AS(SELECT author, title, articles.id AS art_id, slug, path, log.id AS log_id FROM articles, log WHERE log.path LIKE '/article/' || articles.slug )")
@@ -14,12 +16,11 @@ def create_view():
 
 
 def execute_query(query):
-    """execute_query takes an SQL query as a parameter.
-       Executes the query and returns the results as a list of tuples.
-      args:
-          query - an SQL query statement to be executed.
-      returns:
-          A list of tuples containing the results of the query.
+    """Execute the query and return the results as a list of tuples.
+    args:
+        query - an SQL query statement to be executed.
+    returns:
+        A list of tuples containing the results of the query.
    """
     try:
         DB = psycopg2.connect("dbname=news")
@@ -33,6 +34,9 @@ def execute_query(query):
 
 
 def question1():
+    """Return the result for question 1;
+    What are the most popular three articles of all time?
+    """
     query = """SELECT title, COUNT(*) AS views FROM articles_log GROUP BY title ORDER BY views DESC LIMIT 3"""
     result = execute_query(query)
     solution = [('\"{}\" - {} views\n'.format(tup[0], tup[1]))
@@ -41,6 +45,9 @@ def question1():
 
 
 def question2():
+    """Return the result for question 2;
+    Who are the most popular article authors of all time?
+    """
     query = """SELECT name, COUNT(*) AS views FROM articles_log, authors WHERE authors.id = articles_log.author GROUP BY name ORDER BY views DESC"""
     result = execute_query(query)
     solution = [('{} - {} views\n'.format(tup[0], tup[1]))
@@ -49,6 +56,9 @@ def question2():
 
 
 def question3():
+    """Return the result for question 3;
+    On which days did more than 1% of requests lead to errors?
+    """
     query = """SELECT * FROM (SELECT log.time:: date AS day, ROUND(100.0 * (SUM(CASE WHEN status SIMILAR TO '(4|5)%' THEN 1 ELSE 0 END)) / COUNT(*), 1) AS errors FROM log GROUP BY day) AS errors_all WHERE errors > 1.0"""
     result = execute_query(query)
     solution = [('{} - {}% errors\n'.format(tup[0].strftime("%B %d, %Y"), tup[1]))
@@ -57,8 +67,9 @@ def question3():
 
 
 def main():
-    # use sys.argv to get the output file path
-    # get solutions text and write/append them into text file.
+    """Use sys.argv to get the output file path
+        and Get solutions text and write/append them into text file.
+    """
     try:
         create_view()
     except:
