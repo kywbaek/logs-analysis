10c85f9315ef04c45734b9445f7738a3e63a2d88
Young Wook Baek <kywbaek@gmail.com>
2017-08-25 17:44:34 +0100

    use format() for outputs in newsdata.py

--- src/newsdata.py
+++ src/newsdata.py
@@ -17,8 +17,7 @@ def question1():
     c = DB.cursor()
     c.execute(
         "SELECT title, COUNT(*) AS views FROM articles_log GROUP BY title ORDER BY views DESC LIMIT 3")
-    solution = [('"' + tup[0] + '" - ' + str(tup[1]) + ' views' + '\n')
-                for tup in c.fetchall()]
+    solution = [('\"{}\" - {} views\n'.format(tup[0], tup[1])) for tup in c.fetchall()]
     DB.close()
     return solution
 
@@ -27,8 +26,7 @@ def question2():
     DB = psycopg2.connect("dbname=news")
     c = DB.cursor()
     c.execute("SELECT name, COUNT(*) AS views FROM articles_log, authors WHERE authors.id = articles_log.author GROUP BY name ORDER BY views DESC")
-    solution = [(tup[0] + ' - ' + str(tup[1]) + ' views' + '\n')
-                for tup in c.fetchall()]
+    solution = [('{} - {} views\n'.format(tup[0], tup[1])) for tup in c.fetchall()]
     DB.close()
     return solution
 
@@ -37,8 +35,7 @@ def question3():
     DB = psycopg2.connect("dbname=news")
     c = DB.cursor()
     c.execute("SELECT * from (SELECT log.time:: date AS day, ROUND(100.0 * (SUM(CASE WHEN status SIMILAR TO '(4|5)%' THEN 1 ELSE 0 END)) / COUNT(*), 1) AS errors FROM log GROUP BY day) AS errors_all WHERE errors > 1.0")
-    solution = [(str(tup[0]) + ' - ' + str(tup[1]) + '% errors' + '\n')
-                for tup in c.fetchall()]
+    solution = [('{} - {}% errors\n'.format(tup[0], tup[1])) for tup in c.fetchall()]
     DB.close()
     return solution
 
